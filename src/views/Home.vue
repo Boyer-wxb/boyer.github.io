03c77ca478a067d5a67e9da7dc481637ab5a67a2
WangXiangbo <xiangbo.wang@hedait.cn>
2020-07-10 13:36:09 +0800

    首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="v">
-    <video src="@/assets/video/1.mp4" controls poster="@/assets/img/1.jpg"></video>
+    <div class='cover' @click="play" v-show="!show">
+      <img src="@/assets/img/1.jpg" alt="" class="video">
+    </div>
+    <iframe src="//player.bilibili.com/player.html?aid=55130548&bvid=BV1q4411N7fV&cid=96400995&page=1" v-show="show"
+      scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true" class="frame"></iframe>
   </div>
 </template>
 
@@ -10,12 +14,17 @@
     name:'Home',
     data(){
       return {
-        
+        show: false,
       }
     },
     created(){
       console.log('I love Cyberpunk 2077!')
     },
+    methods:{
+      play(){
+        this.show = true
+      }
+    }
   }
 </script>
 
@@ -27,7 +36,17 @@ $footer: 50px;
   height: calc(100vh - #{$header} - #{$footer});
   overflow: hidden;
   background: black;
-  video{
+  .cover{
+    width: 100%;
+    height: inherit;
+    align-items: center;
+    display: flex;
+    cursor: pointer;
+    .video{
+      width: 100%;
+    }
+  }
+  .frame{
     width: 100%;
     height: inherit;
   }
